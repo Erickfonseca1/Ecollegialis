77e631710e73e16a0925833a41015a0863b0d471
Erick Fonseca <erickboyfla@gmail.com>
2023-08-21 13:56:22 -0300

    create initial project

---
+++ src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/ecollegialis/EcollegialisApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.tsi.pweb2.ecollegialis.ecollegialis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcollegialisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcollegialisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/tsi/pweb2/ecollegialis/ecollegialis/EcollegialisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.tsi.pweb2.ecollegialis.ecollegialis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcollegialisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
