1ed49e5a5fd8c73894b04b6602554b4ce8a25a28
Ronaldo Paulo da Silva Filho <ronaldo.filho@assert.ifpb.edu.br>
2023-10-24 22:40:45 -0300

    Up

--- src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/controller/ColegiadoController.java
+++ src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/controller/ColegiadoController.java
@@ -1,5 +1,6 @@
 package br.edu.ifpb.tsi.pweb2.ecollegialis.controller;
 
+import br.edu.ifpb.tsi.pweb2.ecollegialis.model.Aluno;
 import br.edu.ifpb.tsi.pweb2.ecollegialis.model.Colegiado;
 import br.edu.ifpb.tsi.pweb2.ecollegialis.model.Professor;
 import br.edu.ifpb.tsi.pweb2.ecollegialis.service.ColegiadoService;
@@ -18,8 +19,42 @@ public class ColegiadoController {
     private final ColegiadoService colegiadoService;
 
     @PostMapping("/criar")
-    public ResponseEntity<Object> criarColegiado(@RequestBody Colegiado colegiado){
+    public ResponseEntity<Object> criarColegiado(@RequestBody Colegiado colegiado) {
         return ResponseEntity.status(HttpStatus.CREATED).body(colegiadoService.criarColegiado(colegiado));
     }
 
+    @PatchMapping("/professor/{idColegiado}/{idProfessor}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public Colegiado adicionarProfessor(@PathVariable Long idColegiado, @PathVariable Long idProfessor){
+        return colegiadoService.adicionarProfessor(idColegiado, idProfessor);
+    }
+
+    @DeleteMapping("/professor/{idColegiado}/{idProfessor}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public Colegiado removerProfessor(@PathVariable int idColegiado, @PathVariable int idProfessor){
+        return colegiadoService.removerProfessor(idColegiado, idProfessor);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public Colegiado buscarColegiadoPorId(@PathVariable Long id) {
+        return colegiadoService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarColegiado(@PathVariable Long id) {
+        colegiadoService.deletarColegiado(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public Colegiado atualizarColegiado(@Valid @RequestBody Colegiado colegiadoAtualizado, @PathVariable Long id) {
+        return colegiadoService.atualizarColegiado(colegiadoAtualizado, id);
+    }
 }
--- src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/controller/ProfessorController.java
+++ src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/controller/ProfessorController.java
@@ -2,12 +2,16 @@ package br.edu.ifpb.tsi.pweb2.ecollegialis.controller;
 
 import br.edu.ifpb.tsi.pweb2.ecollegialis.model.Aluno;
 import br.edu.ifpb.tsi.pweb2.ecollegialis.model.Professor;
+import br.edu.ifpb.tsi.pweb2.ecollegialis.model.Reuniao;
+import br.edu.ifpb.tsi.pweb2.ecollegialis.model.Usuario;
 import br.edu.ifpb.tsi.pweb2.ecollegialis.service.ProfessorService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -20,4 +24,26 @@ public class ProfessorController {
     public ResponseEntity<Object> criarProfessor(@RequestBody Professor professor){
         return ResponseEntity.status(HttpStatus.CREATED).body(professorService.criarProfessor(professor));
     }
+
+    @GetMapping("/listar")
+    public ResponseEntity<Object> listarProfessores(){
+        return ResponseEntity.ok(professorService.listarProfessores());
+    }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<Object> buscarProfessorPorId(@PathVariable Long id){
+        return ResponseEntity.ok(professorService.buscarProfessorPorId(id));
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Object> deletarProfessor(@PathVariable Long id){
+        professorService.deletarProfessor(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<Object> atualizarProfessor(@Valid @RequestBody Professor professor){
+        return ResponseEntity.ok(professorService.atualizarProfessor(professor));
+    }
+
 }
