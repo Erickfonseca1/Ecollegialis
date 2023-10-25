87b6925f4ae4ad6160ed7cbf926e5be34c5d70ad
Ronaldo Paulo da Silva Filho <ronaldo.filho@assert.ifpb.edu.br>
2023-10-24 21:45:58 -0300

    add assunto controller

--- src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/controller/AssuntoController.java
+++ src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/controller/AssuntoController.java
@@ -5,10 +5,7 @@ import br.edu.ifpb.tsi.pweb2.ecollegialis.service.AssuntoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/assuntos")
@@ -22,6 +19,25 @@ public class AssuntoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(assuntoService.criarAssunto(assunto));
     }
 
+    @GetMapping("/buscar-por-nome/{nome}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public Assunto buscarAssuntoPorNome(@PathVariable String nome) {
+        return assuntoService.buscarAssuntoPorNome(nome);
+    }
 
+    @DeleteMapping("/deletar-por-nome/{nome}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Object> deletarAssuntoPorNome(@PathVariable String nome) {
+        assuntoService.deletarAssuntoPorNome(nome);
+        return ResponseEntity.status(HttpStatus.OK).body("Assunto deletado com sucesso!");
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public Assunto atualizarAssunto(@RequestBody Assunto assuntoAtualizado) {
+        return assuntoService.atualizarAssunto(assuntoAtualizado);
+    }
 
 }
