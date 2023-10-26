26b0c377fb81e50660871aadee89326fb2da37df
Ronaldo Paulo da Silva Filho <ronaldo.filho@assert.ifpb.edu.br>
2023-10-26 09:41:49 -0300

    Add Validations

--- src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/model/Usuario.java
+++ src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/model/Usuario.java
@@ -2,6 +2,8 @@ package br.edu.ifpb.tsi.pweb2.ecollegialis.model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +18,12 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank(message = "Campo obrigatório")
     private String nome;
     private String fone;
+    @NotBlank(message = "Campo obrigatório")
     private String matricula;
+    @NotBlank(message = "Campo obrigatório")
+    @Size(min = 3, max = 60, message = "Senha deve ter entre 3 e 60 caracteres")
     private String senha;
 }
