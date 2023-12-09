3358b446f115b3115c9f735c49c2593577a7f179
Erick Fonseca <erickboyfla@gmail.com>
2023-12-09 18:16:40 -0300

    chore: adjusts

--- src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/controller/ProcessoController.java
+++ src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/controller/ProcessoController.java
@@ -3,8 +3,11 @@ package br.edu.ifpb.tsi.pweb2.ecollegialis.controller;
 
 import java.util.List;
 
+import javax.naming.Binding;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,7 @@ import br.edu.ifpb.tsi.pweb2.ecollegialis.model.Assunto;
 import br.edu.ifpb.tsi.pweb2.ecollegialis.model.Processo;
 import br.edu.ifpb.tsi.pweb2.ecollegialis.service.AssuntoService;
 import br.edu.ifpb.tsi.pweb2.ecollegialis.service.ProcessoService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/processo")
@@ -34,15 +38,18 @@ public class ProcessoController {
     return mv;
   }
 
-  @GetMapping("/list")
-  public ModelAndView listaProcessos() {
-    ModelAndView mv = new ModelAndView("Processo/listaProcessos");
-    return mv;
-  }
+  @PostMapping("/salvar-processo")
+  public String salvarProcesso(@Valid Processo processo, BindingResult result) {
+    if (result.hasErrors()) {
+      System.out.println(result.getAllErrors());
+      return "Processo/formProcesso";
+    }
+    if (processo.getId() != null) {
+      processoService.update(processo);
+    } else {
+      processoService.save(processo);
+    }
 
-  @GetMapping("/edit")
-  public ModelAndView editarProcesso() {
-    ModelAndView mv = new ModelAndView("Processo/formProcesso");
-    return mv;
+    return "redirect:/processo/list";
   }
 }
