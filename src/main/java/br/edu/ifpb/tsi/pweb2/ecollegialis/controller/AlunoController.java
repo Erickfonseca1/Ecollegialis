89ea96e7b603e5b294c0f3dc44bc55b9999aa28a
Ronaldo Paulo da Silva Filho <ronaldopsilvafilho@gmail.com>
2023-12-11 20:47:38 -0300

    Fix controller Aluno

--- src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/controller/AlunoController.java
+++ src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/controller/AlunoController.java
@@ -2,6 +2,7 @@ package br.edu.ifpb.tsi.pweb2.ecollegialis.controller;
 
 import br.edu.ifpb.tsi.pweb2.ecollegialis.model.Aluno;
 import br.edu.ifpb.tsi.pweb2.ecollegialis.service.AlunoService;
+import br.edu.ifpb.tsi.pweb2.ecollegialis.service.CursoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,24 +10,30 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import java.util.List;
 
 @Controller
-@RequestMapping("/aluno")
+@RequestMapping("/alunos")
 public class AlunoController {
 
     @Autowired
     private AlunoService alunoService;
 
+    @Autowired
+    private CursoService cursoService;
+
     @GetMapping
-    public ModelAndView listAlunos(ModelAndView model){
-        model.addObject("aluno", alunoService.getAlunos());
+    public ModelAndView listAlunos(ModelAndView model) {
+        model.addObject("alunos", alunoService.getAlunos());
         model.setViewName("Aluno/listaAlunos");
         return model;
     }
+
     @GetMapping("criar")
-    public ModelAndView createAluno(ModelAndView model, RedirectAttributes redirectAttributes ){
+    public ModelAndView createAluno(ModelAndView model, RedirectAttributes redirectAttributes) {
         model.addObject("aluno", new Aluno());
+        model.addObject("cursos", cursoService.getCursos());
         model.addObject("acao", "salvar");
         model.setViewName("Aluno/formAluno");
         return model;
@@ -38,52 +45,56 @@ public class AlunoController {
             BindingResult validation,
             ModelAndView model,
             RedirectAttributes redirectAttributes
-    ){
+    ) {
         if (validation.hasErrors()) {
             model.setViewName("Aluno/formAluno");
             return model;
         }
         alunoService.salvarAluno(aluno);
-        model.addObject("aluno", alunoService.getAlunos());
-        model.setViewName("redirect:/aluno");
-        redirectAttributes.addFlashAttribute("mensagem", "Aluno Criado com Sucesso");
+        model.addObject("alunos", alunoService.getAlunos());
+        model.setViewName("redirect:/alunos");
+        redirectAttributes.addFlashAttribute("mensagem", "O Aluno foi criado!");
         return model;
     }
 
     @GetMapping("{id}")
-    public ModelAndView editAluno(@PathVariable("id") Long id, ModelAndView model, RedirectAttributes redirectAttributes){
-        Aluno aluno = alunoService.getAlunoPorId(id);
-        model.addObject("aluno", aluno);
+    public ModelAndView editAluno(@PathVariable("id") Long id, ModelAndView model, RedirectAttributes redirectAttributes) {
+        model.addObject("aluno", alunoService.getAlunoPorId(id));
+        model.addObject("cursos", this.cursoService.getCursos());
         model.addObject("acao", "editar");
         model.setViewName("Aluno/formAluno");
+        redirectAttributes.addFlashAttribute("mensagem", "O Aluno foi Editado!");
+        redirectAttributes.addFlashAttribute("O Aluno foi Editado", true);
         return model;
     }
 
     @PostMapping("{id}")
     public ModelAndView updateAluno(
-            @PathVariable("id") Long id,
             @Valid Aluno aluno,
             BindingResult validation,
+            @PathVariable("id") Long id,
             ModelAndView model,
-            RedirectAttributes redirectAttributes
-    ){
+            RedirectAttributes redirectAttributes) {
         if (validation.hasErrors()) {
-            model.setViewName("Aluno/formAluno");
+            model.addObject("aluno", alunoService.getAlunoPorId(id));
+            model.setViewName("Aluno/formAluno"+id);
             return model;
         }
         alunoService.salvarAluno(aluno);
-        model.addObject("aluno", alunoService.getAlunos());
-        model.setViewName("redirect:/aluno");
-        redirectAttributes.addFlashAttribute("mensagem", "Aluno Atualizado com Sucesso");
+        model.addObject("alunos", alunoService.getAlunos());
+        model.setViewName("redirect:/alunos");
+        redirectAttributes.addFlashAttribute("mensagem", "O Aluno foi atualizado!");
+        redirectAttributes.addFlashAttribute("O Aluno foi Salvo", true);
         return model;
     }
 
     @GetMapping("{id}/delete")
-    public ModelAndView deleteAluno(@PathVariable("id") Long id, ModelAndView model, RedirectAttributes redirectAttributes){
+    public ModelAndView deleteAluno(@PathVariable("id") Long id, ModelAndView model, RedirectAttributes redirectAttributes) {
         alunoService.apagarAluno(id);
-        model.addObject("aluno", alunoService.getAlunos());
-        model.setViewName("redirect:/aluno");
-        redirectAttributes.addFlashAttribute("mensagem", "Aluno Deletado com Sucesso");
+        model.addObject("alunos", alunoService.getAlunos());
+        model.setViewName("redirect:/alunos");
+        redirectAttributes.addFlashAttribute("mensagem", "O Aluno foi deletado!");
+        redirectAttributes.addFlashAttribute("O Aluno foi deletado!", true);
         return model;
     }
 
