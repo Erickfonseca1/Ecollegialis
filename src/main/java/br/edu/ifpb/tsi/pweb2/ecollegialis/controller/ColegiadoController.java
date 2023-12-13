9e5d67292588fe042289dd01dab55e8e29e5d0d1
Erick Fonseca <erickboyfla@gmail.com>
2023-12-13 01:32:48 -0300

    feat: colegiado pages

--- src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/controller/ColegiadoController.java
+++ src/main/java/br/edu/ifpb/tsi/pweb2/ecollegialis/controller/ColegiadoController.java
@@ -23,26 +23,87 @@ public class ColegiadoController {
     @Autowired
     private ProfessorService professorService;
 
+    @Autowired
+    private CursoService cursoService;
+
+    @Autowired
+    private CoordenadorService coordenadorService;
+
     @ModelAttribute("professores")
     public List<Professor> getProfessores(){
         return this.professorService.getProfessores();
     }
 
-    @GetMapping
-    public ModelAndView listColegiados(ModelAndView model){
+    @GetMapping("professor/{id}")
+    public ModelAndView listColegiados(@PathVariable("id")Long id, ModelAndView model){
+        Coordenador coordenador = this.coordenadorService.getCoordenadorPorId(id);
+        model.addObject("coordenador", coordenador);
         model.addObject("colegiados", colegiadoService.getColegiados());
         model.setViewName("Colegiado/listaColegiados");
         return model;
     }
 
+    @GetMapping("criar/{id}")
+    public ModelAndView createColegiado(ModelAndView model, @PathVariable("id") Long id ,RedirectAttributes redirectAttributes ){
+        Coordenador coordenador = coordenadorService.getCoordenadorPorId(id);
+
+        //listar professores que são do mesmo curso do coordenador, excluindo o coordenador
+        List<Professor> professores = professorService.getProfessoresPorCurso(coordenador.getCurso().getId());
+        professores.remove(coordenador.getProfessor());
+
+        //curso do coordenador
+        Curso curso = coordenador.getCurso();
+
+        model.addObject("curso", curso);
+        model.addObject("coordenador", coordenador);
+        model.addObject("professores", professores);
+        model.addObject("colegiado", new Colegiado(professores));
+        model.addObject("acao", "salvar");
+        model.setViewName("Colegiado/formColegiado");
+        return model;
+    }
+
+    @PostMapping("criar/{id}")
+    public ModelAndView saveColegiado(
+            @Valid Colegiado colegiado,
+            BindingResult validation,
+            @PathVariable("id") Long id,
+            ModelAndView model,
+            RedirectAttributes redirectAttributes
+    ){
+        if (validation.hasErrors()) {
+            Coordenador coordenador = coordenadorService.getCoordenadorPorId(id);
+
+            //listar professores que são do mesmo curso do coordenador
+            List<Professor> professores = professorService.getProfessoresPorCurso(coordenador.getCurso().getId());
+
+            //curso do coordenador
+            Curso curso = coordenador.getCurso();
+
+            model.addObject("curso", curso);
+            model.addObject("coordenador", coordenador);
+            model.addObject("professores", professores);
+            model.addObject("colegiado", new Colegiado(professores));
+            model.addObject("acao", "salvar");
+            model.setViewName("Colegiado/formColegiado");
+            return model;
+        }
+        colegiadoService.salvarColegiado(colegiado);
+        model.addObject("colegiados", colegiadoService.getColegiados());
+        model.setViewName("redirect:/colegiados");
+        redirectAttributes.addFlashAttribute("mensagem", "Colegiado Criado com Sucesso");
+        redirectAttributes.addFlashAttribute("colegiadosSalvo", true);
+        return model;
+    }
+
     @GetMapping("criar")
     public ModelAndView createColegiado(ModelAndView model, RedirectAttributes redirectAttributes ){
-        List<Professor> membros = new ArrayList<>();
+        List<Professor> membros = new ArrayList<Professor>();
         for(int i=0 ; i<4;i++){
             membros.add(new Professor());
         }
-        model.addObject("colegiado", new Colegiado(membros));
         model.addObject("membros", membros);
+        model.addObject("colegiado", new Colegiado(membros));
         model.addObject("acao", "salvar");
         model.setViewName("Colegiado/formColegiado");
         return model;
